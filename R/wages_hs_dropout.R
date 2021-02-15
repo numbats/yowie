6a5524a032303811aa5bb7c88d15f57a4086f558
Dewi-Amaliah <dama0007@student.monash.edu>
2021-02-16 01:07:20 +1100

    Added documentation for hs dropout data.

--- R/wages_hs_dropout.R
+++ R/wages_hs_dropout.R
@@ -1,14 +1,15 @@
-#' Wages Data from National Longitudinal Survey of Youth
+#' Wages Data of High School Dropout from The NLSY79
 #'
 #' A data set contains longitudinal data of mean hourly wages along
 #' with several demographic variables from National Longitudinal Survey of Youth (NLSY)
 #' from Round 1 (1979 survey year) to Round 28 (2018 survey year).
-#' The cohort provided in this data set are those with maximum highest grade completed is
-#' high school and participated at least 5 years in the survey.
+#' The cohort provided in this data set are high school dropout i.e the cohort who only completed
+#' either 9th, 10th, 11th grade, or at least 19 years old when completed high school.
 #'
 #' @source [The NLSY79 Database](https://www.nlsinfo.org/content/cohorts/nlsy79/get-data)
 #'
-#' @format A data frame with 103,994 rows and 15 variables:
+#' @format A data frame contains 97,087 rows and 15 variables:
+#'
 #' \describe{
 #'   \item{id}{A unique individual's ID number. This is the `key` of the data.}
 #'   \item{year}{The year the observation was taken. This could be the `index` of the data.}
@@ -35,21 +36,4 @@
 #'                FALSE = The id is not extreme.}
 #' }
 #'
-#' @examples
-#' # show the data
-#' wages_hs2020
-#' # turn the data into a tsibble object
-#' library(brolgar)
-#' wages <- as_tsibble(x = wages_hs2020,
-#'                     key = id,
-#'                     index = year,
-#'                     regular = FALSE)
-#' # create the spaghetti plot of the data
-#' library(ggplot2)
-#' ggplot(wages) +
-#'     geom_line(aes(x = year,
-#'                   y = mean_hourly_wage,
-#'                   group = id), alpha = 0.1)
-#'
-
-"wages_hs2020"
+"wages_hs_dropout"
